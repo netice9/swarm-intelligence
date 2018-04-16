d76dc7c6c2fe8b4412ff890e8c6305dbb71e5eee
Dragan Milic <dragan@netice9.com>
2018-04-16 22:35:23 +0100

    added pie chart for cpu and ram per napespace

--- frontend/src/components/namespaces.js
+++ frontend/src/components/namespaces.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import _ from 'lodash'
 import filesize from 'filesize'
+import { Chart } from 'react-google-charts'
 
 class Index extends Component {
 
@@ -18,30 +19,70 @@ class Index extends Component {
 
     const { modal } = this.state
 
+    const memoryData = _.map(this.props.swarm.namespaces,(ns) => [ns.namespace,ns.memory])
+
+    const cpuData = _.map(this.props.swarm.namespaces,(ns) => [ns.namespace,ns.cpu*100])
+
+
     return (
       <div>
-        <div className="container">        
-          <h3>Namespaces</h3>
-          <table className="table table-striped table-hover">
-            <thead className="thead">
-              <tr>
-                <th>Name</th>
-                <th style={ {textAlign: 'right'} } >Memory Usage</th>
-                <th style={ {textAlign: 'right'} } >% CPU Usage</th>
-              </tr>
-            </thead>
-            <tbody>
-            {
-              _.map(this.props.swarm.namespaces, (ns) => (
-                <tr key={ns.namespace} onClick={ () => {this.props.history.push(`/namespaces/${ns.namespace}`)}}>
-                  <td>{ns.namespace}</td>
-                  <td style={ {textAlign: 'right'} } >{filesize(ns.memory || 0)}</td>
-                  <td style={ {textAlign: 'right'} } >{(ns.cpu * 100).toFixed(2)}</td>
-                </tr>
-              ))
-              }
-            </tbody>
-          </table>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h3>Namespaces</h3>
+              <table className="table table-striped table-hover">
+                <thead className="thead">
+                  <tr>
+                    <th>Name</th>
+                    <th style={ {textAlign: 'right'} } >Memory Usage</th>
+                    <th style={ {textAlign: 'right'} } >% CPU Usage</th>
+                  </tr>
+                </thead>
+                <tbody>
+                {
+                  _.map(this.props.swarm.namespaces, (ns) => (
+                    <tr key={ns.namespace} onClick={ () => {this.props.history.push(`/namespaces/${ns.namespace}`)}}>
+                      <td>{ns.namespace}</td>
+                      <td style={ {textAlign: 'right'} } >{filesize(ns.memory || 0)}</td>
+                      <td style={ {textAlign: 'right'} } >{(ns.cpu * 100).toFixed(2)}</td>
+                    </tr>
+                  ))
+                  }
+                </tbody>
+              </table>
+              </div>
+            </div>
+
+          <div className="row">
+            <div className="col">
+              <Chart
+                chartType="PieChart"
+                data={[
+                  ['Namespace', 'Bytes Used'],
+                  ...memoryData
+                ]}
+                options={ {title: "Memory Usage"} }
+                pieHole={0.4}
+                width="100%"
+                graph_id="MemoryChart"
+              />
+            </div>
+
+            <div className="col">
+              <Chart
+                chartType="PieChart"
+                data={[
+                  ['Namespace', '%CPU'],
+                  ...cpuData
+                ]}
+                options={ {title: "CPU Usage"} }
+                width="100%"
+                pieHole={0.4}
+                graph_id="CPUChart"
+              />
+            </div>
+          </div>
+
         </div>
       </div>
     )
